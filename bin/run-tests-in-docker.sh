16cb983f9845356dff2e49f8be7249570c00dd92
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2025-06-05 19:40:01 +0200

    Revert read-only flag

--- bin/run-tests-in-docker.sh
+++ bin/run-tests-in-docker.sh
@@ -19,6 +19,7 @@ docker build --rm -t exercism/test-runner .
 # Run the Docker image using the settings mimicking the production environment
 docker run \
     --network none \
+    --read-only \
     --mount type=bind,src="${PWD}/tests",dst=/opt/test-runner/tests \
     --mount type=tmpfs,dst=/tmp \
     --workdir /opt/test-runner \
